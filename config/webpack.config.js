f85d02c08569d560a1b64b75387082837d6bd4e2
ZY <1822545638@qq.com>
2023-01-26 01:07:27 +0800

    feat#合并开发环境和生产环境的webpack配置

---
+++ config/webpack.config.js
@@ -0,0 +1,173 @@
+const path = require('path');
+const EslintWebpackPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 提取css成单独文件
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin'); // css压缩
+const TerserWebpackPlugin = require('terser-webpack-plugin'); // js代码压缩
+const ImageMinimizerWebpackPlugin = require('image-minimizer-webpack-plugin'); // 图片压缩
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+console.log('---process.env.NODE_ENV:', process.env.NODE_ENV);
+// 获取cross-env获取的环境变量
+const isProduction = process.env.NODE_ENV === 'production';
+
+// process.env.NODE_ENV = 'development';
+// 返回处理样式loader的函数
+const getStyleLoaders = (pre) => {
+    return [
+        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+        'css-loader',
+        {
+            // 处理css兼容性问题
+            // 配合package.json中的browserslist来指定兼容性（做到什么程度）
+            loader: 'postcss-loader',
+            options: {
+                postcssOptions: {
+                    plugins: ['postcss-preset-env']
+                }
+            }
+        },
+        pre
+    ].filter(Boolean);
+}
+
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: isProduction ? path.resolve(__dirname, '../dist') : undefined,
+        filename: isProduction ? 'static/js/[name].[contenthash:10].js' : 'static/js/[name].js',
+        chunkFilename: isProduction ? 'static/js/[name].[contenthash:10].chunk.js' : 'static/js/[name].chunk.js',
+        assetModuleFilename: 'static/media/[hash:10][ext][query]',
+    },
+    module: {
+        rules: [
+            // 处理css
+            {
+                test: /\.css$/,
+                use: getStyleLoaders(),
+            },
+            {
+                test: /\.less$/,
+                use: getStyleLoaders("less-loader"),
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: getStyleLoaders("sass-loader"),
+            },
+            // 处理图片
+            {
+                test: /\.(jpe?g|gif|png|webp|svg)/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 10 * 1024,
+                    }
+                }
+            },
+            {
+                test: /\.(woff2?|ttf)$/,
+                type: 'asset/resource'
+            },
+            // 处理js
+            {
+                test: /\.jsx?$/,
+                include: path.resolve(__dirname,'../src'),
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true, // 开启缓存
+                    cacheCompression: false, // 不开启压缩
+                    plugins: [
+                        !isProduction && 'react-refresh/babel'
+                    ].filter(Boolean), // 激活js的HMR
+                },
+            }
+        ]
+    },
+    plugins: [
+        new EslintWebpackPlugin({
+            context: path.resolve(__dirname, '../src'), // 指定文件根目录
+            exclude: 'node_modules',
+            cache: true,
+            cacheLocation: path.resolve(__dirname, '../node_modules/.cache/.eslintcache'),
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html'),
+        }),
+        isProduction && new MiniCssExtractPlugin({
+            filename: 'static/css/[name].[contenthash:10].css',
+            chunkFilename: 'static/css/[name].[contenthash:10].chunk.css',
+        }),
+        isProduction && new ImageMinimizerWebpackPlugin({
+            minimizer: {
+                implementation: ImageMinimizerWebpackPlugin.imageminGenerate,
+                options: {
+                    plugins: [
+                        ['gifsicle', { interlaced: true }],
+                        ['jpegtran', { progressive: true }],
+                        ['optipng', {optimizationLevel: 5 }],
+                        [
+                            "svgo", {
+                                plugins: [
+                                    "preset-default",
+                                    "prefixIds",
+                                    {
+                                        name: 'sortAttrs',
+                                        params: {
+                                            xmlnsOrder: 'alphabetical',
+                                        }
+                                    }
+                                ],
+                            }
+                        ]
+                    ],
+                }
+            }
+        }),
+        isProduction && new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, '../public'),
+                    to: path.resolve(__dirname, '../dist'),
+                    globOptions: {
+                        // 忽略index.html文件
+                        ignore: ["**/index.html"]
+                    }
+                }
+            ],
+
+        }),
+        !isProduction && new ReactRefreshWebpackPlugin(), // 激活js的HMR
+    ].filter(Boolean),
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        runtimeChunk: {
+            name: (entryPoint) => `runtime~${entryPoint.name}`
+        },
+        // 是否需要进行压缩
+        minimize: isProduction, // true：使用minimizer的配置，false：不使用minimizer的配置
+        minimizer: [
+            new CssMinimizerWebpackPlugin(), // css的压缩
+            new TerserWebpackPlugin(), // js代码压缩
+        ],
+    },
+    // webpack解析模块加载选项
+    resolve: {
+        // 自动补全文件扩展名
+        extensions: [".jsx", ".js", ".json"]
+    },
+    // 需要运行指令时加server 参数，才会激活devServer配置
+    devServer: {
+        host: 'localhost',
+        port: 3002,
+        open: true,
+        hot: true, // 开启HMR
+        historyApiFallback: true, // 解决前端路由刷新404问题
+    }
+}
