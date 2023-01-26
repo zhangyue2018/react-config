ddad45f2e70cdbe5faa415bf0eea67b896ba3c8e
ZY <1822545638@qq.com>
2023-01-26 16:01:57 +0800

    feat#更改主题色

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { Suspense, lazy } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
+import { Button } from 'antd';
 // import Home from './pages/Home';
 // import About from './pages/About';
 
 const Home = lazy(() => import(/*webpackChunkName: 'home'*/'./pages/Home'));
 const About = lazy(() => import(/*webpackChunkName: 'about'*/'./pages/About'));
 
-
 function App() {
     return <div>
         <h1>hello word</h1>
+            <Button type="primary">Primary Button</Button>
         <ul>
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/about">About</Link></li>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 // import App from './App';
 import App from './App.jsx';
+import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
     <BrowserRouter>
-        <App />
+        <ConfigProvider 
+            theme={{
+                token: {
+                    colorPrimary: '#00b96b',
+                }
+            }}
+        >
+            <App />
+        </ConfigProvider>
     </BrowserRouter>
 );
